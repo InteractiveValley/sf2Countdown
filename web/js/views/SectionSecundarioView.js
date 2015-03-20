e0a06a1a09adf876c61615aac5650e0bb413054a
Richpolis Systems <richpolis@gmail.com>
2015-03-20 15:53:44 -0600

    Cambios de frontend y diseño

--- web/js/views/SectionSecundarioView.js
+++ web/js/views/SectionSecundarioView.js
@@ -2,15 +2,12 @@ define([
     'jquery',
     'underscore',
     'Backbone',
-    'models/FiltroPrecioModel',
-    'collections/ColoresCollection',
     'views/ItemProductoView',
     'text!templates/SectionSecundarioView.tpl',
     'bootstrap',
-    'bootstrap-slider',
     'jquery.masonry.min'
 ],
-        function ($, _, Backbone, FiltroPrecioModel, ColoresCollection, ItemProductoView, SectionSecundarioViewTemplate) {
+        function ($, _, Backbone, ItemProductoView, SectionSecundarioViewTemplate) {
             var SectionSecundarioView = Backbone.View.extend({
                 tagName: 'section',
                 template: _.template(SectionSecundarioViewTemplate),
@@ -18,12 +15,8 @@ define([
                     console.log('inicializando sectionsecundarioview');
                     this.id = 'secundario';
                     this.status = '';
-                    this.filtroPrecio = new FiltroPrecioModel();
-                    this.colores = new ColoresCollection();
-                    this.colores.fetch();
                     this.collection.on('add', this.addOne, this);
                     this.collection.on('reset', this.render, this);
-                    this.filtroPrecio.on('change',this.filtrar, this);
                 },
                 events: {
                     
@@ -36,7 +29,6 @@ define([
                     this.$el.html(this.template());
                     this.status = 'render';
                     this.collection.forEach(this.addOne, this);
-                    this.sliderPrecio();
                     this.status = '';
                     return this;
                 },
@@ -52,23 +44,6 @@ define([
                     for(var i = app.collections.productos.length -1; i>=0;i--){
                         app.collections.productos.models[i].destroy();
                     }
-                },
-                sliderPrecio: function(){
-                    var self = this;
-                    this.$el.find("#sliderPrecio").slider({'tooltip': 'show'});
-                    this.$el.find(".slider-horizontal").css({'width': '100%'});
-                    this.$el.find("#valor-precio").text( formatNumber.new(2000,"$"));
-                    this.$el.find("#sliderPrecio").on('slide', function (ev) {
-                        self.$el.find("#valor-precio").text( formatNumber.new(ev.value,"$"));
-                        self.filtroPrecio.set({'value': ev.value});
-                    }).on("slideStop", function (ev) {
-
-                    });
-                },
-                filtrar: function(){
-                    
-                    this.collection.filtrarPorPrecio(this.filtroPrecio.get('value'));
-                    
                 },
                 masonry: function(){
                     this.$el.find('.productos').masonry({
